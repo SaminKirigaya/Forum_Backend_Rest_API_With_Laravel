8208579adeffda0905c31e07723e35dec618e1f9
SaminKirigaya <arnobsamin95@gmail.com>
2023-07-07 04:59:16 +0600

    Bug fix

--- app/Http/Controllers/Changepasspage.php
+++ app/Http/Controllers/Changepasspage.php
@@ -13,12 +13,21 @@ class Changepasspage extends Controller
         
             $is_logged_in = DB::table('tokendb')->where('token',$tokenz)->count()>0; //lets see if he has token in db
             if($is_logged_in){//he has it now
-                $all_data = DB::table('users')->select('*')->where('slno',$usersl)->get(); //taking all user slno related data
-                
-                return response()->json([
-                    'message'=>'Successful',
+                $tok_email = DB::table('tokendb')->select('user_email')->where('token',$tokenz)->first();
+                $usrsl_email = DB::table('users')->select('email')->where('slno',$usersl)->first();
+                if($tok_email->user_email == $usrsl_email->email){
+                    $all_data = DB::table('users')->select('*')->where('slno',$usersl)->get(); //taking all user slno related data
                     
-                ], 200);// sent datas
+                    return response()->json([
+                        'message'=>'Successful',
+                        
+                    ], 200);// sent datas
+                }else{
+                    return response()->json([
+                        'message'=>'Invalid Token According To Serial.',
+                        
+                    ], 200);
+                }    
             }else{
                 return response()->json([
                     'message' => 'Please Log In First To See Profile.',
